d4ca2f18a1a27ca0cdab04e2a11db780005d338d
Adria Cidre <adria.cidre@gmail.com>
2016-09-15 13:15:53 +0200

    Split messages on bootstraps.create + executions.create

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-BASH_GO_MODE=sync $GOPATH/bin/bash-nats execution.create.salt ruby adapter.rb
+BASH_GO_MODE=sync $GOPATH/bin/bash-nats execution.create.salt ruby adapter.rb && BASH_GO_MODE=sync $GOPATH/bin/bash-nats bootstrap.create.salt ruby adapter.rb
